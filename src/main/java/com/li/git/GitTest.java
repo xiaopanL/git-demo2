10fb52f08a3280c8593a50d18bf412d3ad7043fa
xiaopang <xiopang@li.com>
2023-03-03 00:05:45 +0800

    3.o

--- src/main/java/com/li/git/GitTest.java
+++ src/main/java/com/li/git/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
 
     }
 
