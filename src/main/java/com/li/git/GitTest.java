eca1628858b3f790065f1c2ca424d387fa77ae2d
xiaopang <xiopang@li.com>
2023-03-06 17:44:59 +0800

    first commit

--- src/main/java/com/li/git/GitTest.java
+++ src/main/java/com/li/git/GitTest.java
@@ -5,6 +5,10 @@ import java.sql.SQLOutput;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
     }
 
 }
